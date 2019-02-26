24d27daec56433d46278bf81392ab54ea89d388f
Ronny Czerkus <thwyster@hotmail.com>
2019-02-26 18:34:29 -0300

    Primeiro Commit

---
+++ Treinamento/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Treinamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Classe2
+    {
+        int variavel1;
+        int variavel2;
+        int variavel3;
+
+        public int propiedade1 { get; set; }
+        public int propiedade2 { get; set; }
+        public int propiedade3 { get; set; }
+
+        public void MetodoPublico() { }
+        private void MetodoPrivado() { }
+        protected void MetodoProtegido() { }
+    }
+
+    class Classe3
+    {
+        public int propiedade4 { get; set; }
+        public int propiedade5 { get; set; }
+        public int propiedade6 { get; set; }
+
+        public void Metodo1()
+        {
+            propiedade4 = 1;
+        }
+    }
+
+    struct Struct1
+    {
+        static int var1 = 1;
+        const int var2 = 1;
+        
+        public void Metodo1()
+        {
+
+        }
+    }
+}
